ab75eb97e8843ed1ed1fc504e6030fd1457af309
Shekcon <quangsang9773@gmail.com>
2018-12-24 14:10:37 +0700

    fix sql

--- 15.sql
+++ 15.sql
@@ -1,5 +1,4 @@
 SELECT first_name, last_name
 FROM persons
-WHERE (nationality = 'Colombia'
-      and shirt_number % 2 = 0)
-      or (nationality != 'France' and shirt_number::TEXT LIKE '%9%');
+WHERE (nationality = 'Colombia' and shirt_number % 2 = 0)
+      or (nationality != 'France' and shirt_number = 9);
